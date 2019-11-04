5b3942fd6c581472e189e64e2e9cce948253967b
Eduardo <abreu.eduardo.r@gmail.com>
2019-11-04 11:54:11 -0500

    Commit script and Update README

---
+++ JPGtoPNGConverter.py
@@ -0,0 +1,27 @@
+'''
+A Python script that converts JPG images to PNG
+'''
+
+# pylint: disable=invalid-name
+
+import sys
+import os
+from PIL import Image
+
+# Grab first and second arguments
+IMAGE_FOLDER = sys.argv[1]
+OUTPUT_FOLDER = sys.argv[2]
+
+# Check if new/exists, if not create it
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
+
+# Loop through 'Pokedex'
+for filename in os.listdir(IMAGE_FOLDER):
+    clean_filename = os.path.splitext(filename)
+
+    img = Image.open(f'{IMAGE_FOLDER}{filename}')
+
+    # Convert images to PNG and save to a new folder
+    img.save(f'{OUTPUT_FOLDER}{clean_filename[0]}.png', 'png')
+    print(f'{filename} converted...')
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# JPGtoPNGConverter
+# JPG to PNG Converter
+
+Python script to convert images to PNG.
+> WARNING: Has only been tested with JPG to PNG
+
+```Bash
+usage: python JPGtoPNGConverter.py <from_this_folder_path> <to_this_folder_path>
+```
